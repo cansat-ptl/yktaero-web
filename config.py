4870dfaad4a64f95d90d8cde9750bd54ef84b882
ThePetrovich <petroff.ap@gmail.com>
2021-07-10 23:12:31 +0900

    Initial commit

---
+++ config.py
@@ -0,0 +1,36 @@
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'oiutsriupg7fyhjlvd78pg9'
+    POSTGRES = {
+        'user': 'postgres',
+        'pw': '',
+        'db': 'test',
+        'host': 'localhost',
+        'port': '5432',
+    }
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TEMPLATES_AUTO_RELOAD = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+CurrentConfig = DevelopmentConfig()
