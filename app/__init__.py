b802b54eb06287a5b8502d751d340cd4cab448ec
ThePetrovich <petroff.ap@gmail.com>
2021-07-11 21:22:38 +0900

    Remove SQLAlchemy dependency

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 import os, sys
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 version_string = "v0.0.1-dev"
 
@@ -11,6 +10,5 @@ from config import CurrentConfig
 
 app = Flask(__name__)
 app.config.from_object(CurrentConfig)
-db = SQLAlchemy(app)
 
 from app import pages
