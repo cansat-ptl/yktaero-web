c7a3d3ab7fe840f79b819f73d46abfaeab7ede96
ThePetrovich <petroff.ap@gmail.com>
2021-08-20 00:34:15 +0900

    Page stubs

--- app/pages.py
+++ app/pages.py
@@ -16,3 +16,31 @@ def index():
 @app.route('/news')
 def news():
     return render_template('news.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+@app.route('/about/staff')
+def staff():
+    return render_template('about/staff.html')
+
+@app.route('/about/partners')
+def partners():
+    return render_template('about/partners.html')
+
+@app.route('/projects/payloads')
+def payloads():
+    return render_template('projects/payloads.html')
+
+@app.route('/projects/yktsat')
+def yktsat():
+    return render_template('projects/yktsat.html')
+
+@app.route('/projects/sounding-rockets')
+def rockets():
+    return render_template('projects/sounding-rockets.html')
+
+@app.route('/projects/ground-equipment')
+def ground():
+    return render_template('projects/ground-equipment.html')
